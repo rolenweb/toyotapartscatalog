09309d39e333c2394dce9744681075a6eac29138
rolenweb <rolenweb@yandex.ru>
2016-12-13 18:40:37 +0300

    setup yii2

---
+++ basic/commands/BaseCommand.php
@@ -0,0 +1,99 @@
+<?php
+namespace app\commands;
+
+use yii\console\Controller;
+use yii\helpers\Console;
+
+class BaseCommand extends Controller
+{
+	protected $time = 0;
+	public $verbose = false;
+	public $trace = false;
+	public $clear = false;
+	protected $asciiGfx = <<<'DOC'
+  ___             _         __      __   _               
+ | _ \__ _ _ _ __(_)_ _  __ \ \    / /__| |__   _ _ _  _ 
+ |  _/ _` | '_(_-< | ' \/ _` \ \/\/ / -_) '_ \_| '_| || |
+ |_| \__,_|_| /__/_|_||_\__, |\_/\_/\___|_.__(_)_|  \_,_|
+                        |___/                            
+DOC;
+	protected $asciiName = <<<'DOC'
+
+
+  ___                        _____             _          ___          _      
+ | _ \__ _ _ _ ___ ___ _ _  |_   _|__ _  _ ___| |_ __ _  | _ \__ _ _ _| |_ ___
+ |  _/ _` | '_(_-</ -_) '_|   | |/ _ \ || / _ \  _/ _` | |  _/ _` | '_|  _(_-<
+ |_| \__,_|_| /__/\___|_|     |_|\___/\_, \___/\__\__,_| |_| \__,_|_|  \__/__/
+                                      |__/                                    
+
+
+DOC;
+
+	public function options($actionID)
+	{
+		return [
+			'verbose',
+			'trace',
+			'clear',
+		];
+	}
+
+	public function optionAliases()
+	{
+		return [
+			'v' => 'verbose',
+			't' => 'trace',
+			'c' => 'clear',
+		];
+	}
+
+	public function beforeAction($action)
+	{
+		if ($this->clear) {
+			Console::clearScreen();
+			echo "\033[3J"; // clear Putty scrollback buffer
+		}
+		Console::output(Console::ansiFormat($this->asciiGfx, [94]));
+		Console::output(Console::ansiFormat($this->asciiName, [92]));
+		return parent::beforeAction($action);
+	}
+
+	protected static function success($message)
+	{
+		Console::output(Console::ansiFormat($message, [Console::FG_GREEN]));
+	}
+
+	protected static function error($message)
+	{
+		Console::output(Console::ansiFormat($message, [Console::FG_RED]));
+	}
+
+	protected function whisper($message)
+	{
+		if ($this->verbose) {
+			Console::output(Console::ansiFormat($message, [90]));
+		}
+	}
+
+	protected function trace($message)
+	{
+		if ($this->trace) {
+			$time = microtime(true);
+			$delta = $time - $this->time;
+			if ($this->time === 0) {
+				$timestr = (new \DateTime)->format('Y-m-d H:i:s');
+			} else {
+				$timestr = number_format($delta, 2, '.', '') . 's';
+			}
+			Console::output(Console::ansiFormat($timestr . ' - ' . $message, [35]));
+			$this->time = $time;
+		}
+	}
+
+	protected function readStdin()
+	{
+		while ($line = fgets(STDIN)) {
+			yield $line;
+		}
+	}
+}
---
+++ basic/migrations/m161213_153338_create_link_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `link`.
+ */
+class m161213_153338_create_link_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%link}}', [
+            'id' => $this->primaryKey(),
+            'url' => $this->string(),
+            'status' => $this->smallInteger(),
+            'created_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP'),
+            'updated_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP'),
+        ], $tableOptions);
+
+        $this->createIndex('idx-link-url', '{{%link}}', 'url');
+    }
+
+    public function down()
+    {
+        $this->dropIndex('idx-link-url', '{{%link}}');
+        $this->dropTable('{{%link}}');
+    }
+}
